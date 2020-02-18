65e7c7ea7df9f91a5d4b463498d4ab15c6dcca28
admin <admin@iMac3-8.local>
2020-02-18 17:23:20 +0100

    Changed profile page, lacked of time and ideas

--- profile.php
+++ profile.php
@@ -8,6 +8,7 @@
     <title>Mythic: Profiles</title>
     <link rel="stylesheet" href="./css/profile.css">
     <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
 </head>
 
 <body>
@@ -18,19 +19,26 @@
     } ?>
 
     <main>
+        <div id="container">
+    
+            <h1>Mon profil</h1>
+            <h1 id="pseudo">Pseudo: <?= $_SESSION['name'] ?></h1>
 
-        <form action="./controllers/settings.php" method="post">
-            <label for="keepSerie">Series
-                <input type="checkbox" value="serieOn" name="serieCheck" <?= ($_SESSION['serieCheck'] == 'serieOn') ? 'checked' : '' ?>>
-            </label>
+            <form action="./controllers/settings.php" method="post">
+                <label for="keepSerie">Series
+                    <input type="checkbox" value="serieOn" name="serieCheck" <?= ($_SESSION['serieCheck'] == 'serieOn') ? 'checked' : '' ?>>
+                </label>
 
-            <label for="keepGame">Jeux Vidéos
-                <input type="checkbox" value="gameOn" name="gameCheck" <?= ($_SESSION['gameCheck'] == 'gameOn') ? 'checked' : '' ?>>
-            </label>
-            <button type="submit">Sauvegarder les préférences</button>
-        </form>
+                <label for="keepGame">Jeux Vidéos
+                    <input type="checkbox" value="gameOn" name="gameCheck" <?= ($_SESSION['gameCheck'] == 'gameOn') ? 'checked' : '' ?>>
+                </label>
+                <button type="submit">Sauvegarder les préférences</button>
+            </form>
 
+        </div>
     </main>
+    <?php include_once './templates/footer.php' ?>
+
 </body>
 
 </html>
